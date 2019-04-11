c64117efe4ffd6cf037014ea09ff40c807dc3935
DraynaRue <mattie0@hotmail.co.uk>
2019-04-11 17:00:28 +0100

    Game Playable On PC, Updated Version Number to 0.9.0a

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour {
+public class CameraScript : MonoBehaviour 
+{
 
     public GameObject player;       //Public variable to store a reference to the player game object
 
@@ -10,13 +11,24 @@ public class CameraScript : MonoBehaviour {
     //private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
-    void Start () {
+    void Start () 
+    {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         //offset = new Vector3(0, );
+        if (Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            GameObject[] _goa;
+            _goa = GameObject.FindGameObjectsWithTag("MobileOnly");
+            for (int i = 0; i < _goa.Length; i++)
+            {
+                _goa[i].SetActive(false);
+            }
+        }
     }
     
     // LateUpdate is called after Update each frame
-    void LateUpdate () {
+    void LateUpdate () 
+    {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
     }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
 	public Rigidbody2D rb;
 	public Animator ani;
 	protected bool facingRight;
@@ -11,7 +12,12 @@ public class PlayerScript : MonoBehaviour {
     static public bool jumping;
     static public float playerHealth;
 
-    void Start() {
+    //PC ONLY VARIABLES
+    public float jumpForce;
+    float horizontal = 0.0f;
+
+    void Start()
+    {
 		rb = GetComponent<Rigidbody2D>();
 		ani = GetComponent<Animator>();
         jumping = false;
@@ -20,20 +26,65 @@ public class PlayerScript : MonoBehaviour {
         ScoreScript.timer = 255;
 	}
 
-	private void FixedUpdate () {
-        if(playerHealth <= 0 || ScoreScript.timer <= 0) {
+	private void FixedUpdate ()
+    {
+        if(playerHealth <= 0 || ScoreScript.timer <= 0)
+        {
             ani.SetBool("isDead", true);
+        }
+
+        //PC ONLY
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                ani.SetBool("isWalking", true);
+                horizontal = Time.deltaTime * -6.0f;
+                Vector3 theScale = rb.transform.localScale;
+                theScale.x = -1.5f;
+                rb.transform.localScale = theScale;
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                ani.SetBool("isWalking", true);
+                horizontal = Time.deltaTime * 6.0f;
+                Vector3 theScale = rb.transform.localScale;
+                theScale.x = 1.5f;
+                rb.transform.localScale = theScale;
+            }
+            else if (Input.GetAxis("Horizontal") == 0)
+            {
+                ani.SetBool("isWalking", false);
+                horizontal = 0.0f;
+            }
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                if(PlayerScript.jumping == false)
+                {
+                    PlayerScript.jumping = true;
+                    rb.velocity = new Vector2(0, jumpForce);
+                    Debug.Log("Jumping:" + PlayerScript.jumping);
+                }
+            }
+            if (Input.GetAxis("Attack") > 0)
+            {
+                ani.SetTrigger("Attack");
+            }
+            rb.transform.Translate(horizontal, 0, 0);
         }
     }
 
-    public void ReturnToSplash () {
+    public void ReturnToSplash ()
+    {
         SceneManager.LoadScene("GameOverScene");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         Debug.Log("Collider tag: " + collision.collider.tag);
         Debug.Log("OtherCollider tag: " + collision.otherCollider.tag);
-        if (collision.collider.tag == "Ground" && jumping == true) {
+        if (collision.collider.tag == "Ground" && jumping == true)
+        {
             jumping = false;
             Debug.Log("Jumping:" + jumping);
         }
